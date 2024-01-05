9999f898eebdae42c3a859ee44c73f2e11ba4893
akmiller01 <alex.k.miller@gmail.com>
2024-01-05 12:56:29 -0500

    More params

--- train_classifier.py
+++ train_classifier.py
@@ -41,9 +41,9 @@ def main(metadata_path):
 
     training_args = TrainingArguments(
         output_dir='models/{}'.format(metadata_basename),
-        learning_rate=1e-5,
-        per_device_train_batch_size=32,
-        per_device_eval_batch_size=32,
+        learning_rate=5e-6,
+        per_device_train_batch_size=48,
+        per_device_eval_batch_size=48,
         num_train_epochs=10,
         weight_decay=0.01,
         evaluation_strategy='epoch',
